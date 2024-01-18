a63ad17b0137db4777a7a8fa10ea02a839680ac2
Udip Rai <atomicdeveloper0329@gmail.com>
2024-01-18 09:01:41 +0545

    managed layouts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,8 +2,9 @@ import type { Metadata } from "next";
 import { RootLayoutSchema } from "@/utils/schemas/CommonSchema";
 import { RootLayoutMetadata } from "@/utils/seo/metadatas/common";
 import { rootLayoutCss } from "@/assets/css/styles/common";
-import "./globals.css";
 import { RootFooter, RootHeader } from "@/dynamic-imports/components";
+import "@/styles/globals.css";
+import Header from "../helpers/components/layouts/root/Header";
 
 // Metadata for SEO
 export const metadata: Metadata = RootLayoutMetadata;
@@ -18,8 +19,9 @@ const RootLayout: React.FC<RootLayoutSchema> = (props) => {
   return (
     <html lang="en">
       <body style={parent}>
-        <div className="bg-red-100 min-w-screen min-h-screen max-w-[2000px] mx-auto grid grid-rows-[144px_minmax(calc(100vh-144px),auto)_400px]">
+        <div className="min-w-screen min-h-screen max-w-[2000px] mx-auto grid grid-rows-[144px_minmax(calc(100vh-144px),auto)_400px]">
           <RootHeader />
+          <Header />
           <main>{children}</main>
           <RootFooter />
         </div>
--- src/helpers/components/layouts/root/Header.tsx
+++ src/helpers/components/layouts/root/Header.tsx
@@ -1,5 +1,7 @@
+import "@/styles/globals.css";
+
 const Header = () => {
-  return <header>Header</header>;
+  return <header className="bg-green-400 w-40 h-56 text-4xl">vsdssd</header>;
 };
 
 export default Header;
