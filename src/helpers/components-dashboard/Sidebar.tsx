2b6dadd8baf5b33877b9d945c91cf43124980642
udip-rai <atomicdeveloper0329@gmail.com>
2024-02-08 23:59:57 +0545

    updated dashboard css

--- src/helpers/components-dashboard/Sidebar.tsx
+++ src/helpers/components-dashboard/Sidebar.tsx
@@ -88,7 +88,7 @@ const Sidebar = ({ sidebarOpen, setSidebarOpen }: SidebarProps) => {
     >
       {/* <!-- SIDEBAR HEADER --> */}
       <div className="flex items-center justify-between gap-2 px-6 py-5.5 lg:py-6.5">
-        <div className="pt-6 px-3">
+        <div className="pt-2.5 px-3">
           <Link href="/">
             <LogoImage />
           </Link>
@@ -108,7 +108,7 @@ const Sidebar = ({ sidebarOpen, setSidebarOpen }: SidebarProps) => {
 
       <div className="no-scrollbar flex flex-col overflow-y-auto duration-300 ease-linear">
         {/* <!-- Sidebar Menu --> */}
-        <nav className="mt-5 px-4 py-4 lg:mt-9 lg:px-6">
+        <nav className="mt-2 px-4 py-4 lg:mt-4 lg:px-6">
           {/* <!-- Menu Group --> */}
           <div>
             <h3 className="mb-4 ml-4 text-sm font-semibold text-bodydark2 cursor-pointer hover:text-white">
